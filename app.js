f9e9946257c54ebf1523d3c5eff4f0e8bc747ef2
vishal <vishal.saini.99154@gmail.com>
2024-09-20 15:37:24 +0530

    server file structure

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const router = require("./router/auth-router");
 app.use(express.json());
 const conectDB = require("./utils/db");
-const userModel = require('./models/user-model.js')
-const chalk = require('chalk');
-
-
+const chalk = require("chalk");
 
 app.use("/api", router);
-
 
 const PORT = 3000;
 conectDB().then(() => {
   app.listen(PORT, () => {
-    
-    console.log(chalk.cyan('Server is running...'));
+    console.log(chalk.cyan("Server is running..."));
   });
 });
