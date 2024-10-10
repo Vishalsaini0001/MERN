a21ce9cb748feb1f6d4964867caeebc5738102bb
vishal <vishal.saini.99154@gmail.com>
2024-10-10 14:39:50 +0530

    page not found added

--- frontend/src/pages/PageNotFound.jsx
+++ frontend/src/pages/PageNotFound.jsx
@@ -7,7 +7,7 @@ const PageNotFound = () => {
         <div className="h-screen  bg-fixed ">
       <video src={videoji} autoPlay loop muted></video>
       </div>
-      <div className="w-full h-screen flex justify-center items-center text-3xl tracking-widest bg-gradient-to-t from-red-300 to-red-200">404 Page Not Found!</div>
+      <div className="w-full h-screen flex justify-center items-center text-3xl tracking-widest bg-myimage1 bg-fixed bg-cover">404 Page Not Found!</div>
       
     </div>
   );
