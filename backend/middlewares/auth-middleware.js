99cefabaccfe3020ee3920251cfe7307f6b6f103
vishal <vishal.saini.99154@gmail.com>
2024-10-15 20:18:37 +0530

    middleware added

--- backend/middlewares/auth-middleware.js
+++ backend/middlewares/auth-middleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 const authmiddleware = (req, res, next)=>{
      const token = req.header("Authorization")
-     console.log("token is....",token)
+     
      next()
 
 }
