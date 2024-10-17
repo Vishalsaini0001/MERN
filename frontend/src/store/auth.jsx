9cebea634df9051635145a787187a1d3b31a9126
vishal <vishal.saini.99154@gmail.com>
2024-10-17 15:04:10 +0530

    context api function for fetching

--- frontend/src/store/auth.jsx
+++ frontend/src/store/auth.jsx
@@ -1,10 +1,12 @@
-import { createContext, useContext, useState } from "react";
+import e from "cors";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext();
 
 
 export const AuthProvider = ({ children }) => {
     const [token, setToken] = useState(localStorage.getItem("token"))
+    const [ userdata, setUser] = useState("")
 
   const setLocalToken = (token) => {
     setToken(token)
@@ -18,8 +20,28 @@ export const AuthProvider = ({ children }) => {
     return localStorage.removeItem("token")
   }
 
+  const userAuthentication = async ()=>{
+  try {
+    const response = await fetch("http://localhost:3000/api/user",{
+      method:"GET",
+      headers:{
+        Authorization:`bearer ${token}`
+      }
+    }) 
+    const data = await response.json();
+    setUser(data);
+  } catch (error) {
+    console.log("authentication error",error)
+  }
+  }
+
+  // get the current logged user
+  useEffect(()=>{
+    userAuthentication();
+  },[])
+
   return (
-    <AuthContext.Provider value={{ setLocalToken,logoutUser,isLoggedIn }}>
+    <AuthContext.Provider value={{ setLocalToken,logoutUser,isLoggedIn,userdata }}>
       {children}
     </AuthContext.Provider>
   );
