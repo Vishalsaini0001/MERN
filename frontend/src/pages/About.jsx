8bdbed78dd93f8c3baec1c9172fd8164903056ee
vishal <vishal.saini.99154@gmail.com>
2024-11-26 12:25:45 +0530

    about chnages

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
                     <h1 className="text-4xl font-semibold leading-9 text-gray-800">Indoor Interiors</h1>
                     <p className="text-base leading-6 mt-4 text-gray-600">Get inspired by our curated selection of luxiwood interiors. We hope get inspired to have luxiwood interior yourself. You’ll find tips here where you can buy a lot of cool furniture.</p>
                     <button aria-label="view catalogue" className="focus:ring-2 focus:ring-offset-2 focus:ring-gray-700 focus:outline-none mt-6 md:mt-8 text-base font-semibold leading-none text-gray-800 flex items-center hover:underline">
-                        View Catalogue
+                        View Catalogu
                         <svg className="ml-2 mt-1" width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M1.33325 4H10.6666" stroke="#1F2937" strokeLinecap="round" strokeLinejoin="round" />
                             <path d="M8 6.66667L10.6667 4" stroke="#1F2937" strokeLinecap="round" strokeLinejoin="round" />
